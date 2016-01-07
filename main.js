9c003547f873d7487b080a9ce9e7c58c5cd19fa1
Brent Nygaard <nygaardmeister@gmail.com>
2016-01-07 15:51:52 +0200

    Begin testing JSON type objects

---
+++ main.js
@@ -0,0 +1,9 @@
+var contact = {
+  'name': 'Brent Nygaard',
+  'tel': '084 047 9471',
+  'email': 'nygaardmeister@gmail.com'
+}
+
+console.log(contact.name)
+console.log(contact.tel)
+console.log(contact.email)
