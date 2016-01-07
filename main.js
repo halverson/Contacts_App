6f6bda608528f942f9992ed0c8d8c10e0d5f3235
Brent Nygaard <nygaardmeister@gmail.com>
2016-01-07 17:24:19 +0200

    Add basic functionality to add a contact. Does not save or persist yet.

--- main.js
+++ main.js
@@ -19,3 +19,16 @@ class Contact {
     console.log('Email Address: ' + this.email)
   }
 }
+
+var option = window.prompt('A - Add new contact  |  P - Print list of existing contacts', 'Type A or P')
+
+if (option.toLowerCase() === 'a') {
+  var newC = new Contact()
+  var name = window.prompt('Type the name and surname of the contact', 'Name Surname')
+  var tel = window.prompt('Type the phone number of the contact', '021 123 4567')
+  var email = window.prompt('Type the email address of the contact', 'name@email.com')
+  newC.name = name
+  newC.tel = tel
+  newC.email = email
+  window.alert('Your new contact: ' + newC.name + ' ' + newC.tel + ' ' + newC.email)
+}
